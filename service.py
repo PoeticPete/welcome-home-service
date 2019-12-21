e4cd76d6c8a1deede26d0dff807fd4c44b85fb92
Peter Tao <peter.tao@viasat.com>
2019-12-20 22:09:43 -0500

    first

---
+++ service.py
@@ -0,0 +1,34 @@
+"""
+1. Keep pinging the list of IPs (python)
+2. Update people that are home (IPs that are reachable)
+3. Front end makes request to backend (http://url.com/people_home)
+4. Return who is home
+
+{
+    "data": [
+        {
+            "name": "Geon",
+            "is_home": true,
+        },
+        {
+            "name": "Mike",
+            "is_home": true,
+        },
+        {
+            "name": "Peter",
+            "is_home": true,
+        },
+    ]
+}
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
