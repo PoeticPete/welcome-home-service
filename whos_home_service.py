eab118fa929c409fa4e6e82f2a7e308b440ca4f5
Peter Tao <peter.tao@viasat.com>
2019-12-21 01:06:23 -0500

    increase timeout

--- whos_home_service.py
+++ whos_home_service.py
@@ -35,7 +35,7 @@ for i in people_ips:
 
 
 # Define being home as "able to ping phone within last 30 minutes (1800 seconds)"
-IS_HOME_TIMEOUT = 20
+IS_HOME_TIMEOUT = 30
 
 def get_whos_home():
     global whos_home
